b36e3169bd2d5ea23371203063f1575145002a2b
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-17 00:53:12 -0300

    Exercício - Separando pares de ímpares

--- extra-15/index.js
+++ extra-15/index.js
@@ -1,4 +1,15 @@
+//Código para separar os números pares e impares do vetor e atribuir em outro vetor conforme sua categoria
 const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = [];
 const impares = [];
 
+for(let i of original){
+
+    if((i % 2) === 0){
+        pares.push(i);
+    }else{
+        impares.push(i);
+    }
+}
+console.log(pares);
+console.log(impares);
