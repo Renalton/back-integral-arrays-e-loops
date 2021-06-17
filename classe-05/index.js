cc3a64c51ba76c46c763b1d1c50edf029cb4831c
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-17 00:49:26 -0300

    Exercício - Filtrando apenas os pares

--- classe-05/index.js
+++ classe-05/index.js
@@ -1,1 +1,12 @@
+//Código para armazenar os valores divisíveis por 2 e armazenar esse valor em outro vetor
 const original = [1, 4, 12, 21, 53, 88, 112];
+//vetor vazio para armazenar os valores encontrados durante o loop
+const vet = [];
+for(let i of original){
+
+    if((i % 2) === 0){
+        vet.push(i)
+    } 
+
+}
+console.log(vet);
