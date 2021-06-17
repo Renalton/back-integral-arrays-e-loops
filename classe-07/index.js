8c7f2878e7957ecde3aa49bcc0f2558d922d8e61
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-17 00:50:04 -0300

    Exercício - Filtrando nomes com a letra A

--- classe-07/index.js
+++ classe-07/index.js
@@ -1,1 +1,15 @@
+//Código para localizar todos nomes que começam com a leta "A" ou "a"
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+//vetor para armazenar todos os nomes localizados
+const vet = [];
+
+for(let nome of nomes){
+
+    if(nome[0] === "A" || nome[0] === "a"){
+        vet.push(nome);
+
+    }
+
+}
+console.log(vet);
+
