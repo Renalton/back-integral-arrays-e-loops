3500106c627de83bb5cd84d39f998a39723a5230
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-17 00:39:27 -0300

    Exercício - Fila do banco

--- extra-12/index.js
+++ extra-12/index.js
@@ -1,3 +1,11 @@
+//Código para controlar a entrada e saida de dados nos vetores sem exceder o limite informado que nesse caso é 5
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
 
+while(filaDeDentro.length < 5 || filaDeFora > 0){
+
+    filaDeDentro.push(filaDeFora.shift());
+
+}
+console.log(filaDeDentro);
+console.log(filaDeFora);
