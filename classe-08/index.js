eb7ec722f07d3023516a0ec903ab823f1e9e8ef6
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-17 00:50:19 -0300

    Exercício - Encontrando o maior

--- classe-08/index.js
+++ classe-08/index.js
@@ -1,1 +1,12 @@
-const numeros = [3, 24, 1, 8, 11, 7, 15];
+//Código para localizar e imprimir o maior valor contido no vetor
+const numeros = [3, 24, 1, 8, 11, 7, 5];
+//Variável para armazenar o maior valor do vetor 
+let eMaior = 0;
+
+for(let numero of numeros){
+
+    if(numero > eMaior){
+        eMaior = numero;
+    }
+}
+console.log(eMaior);
