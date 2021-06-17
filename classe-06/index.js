4013f6f823e8d29a9957cae72b4e2d397a73ddfb
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-17 00:49:42 -0300

    Exercício - Soma dos pares

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,2 +1,12 @@
+//Código para somar todos os números pares de um vetor
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+//variável para somar os numeros pares do vetor
+let somapar = 0;
 
+for(let i = 0; i < numeros.length; i++){
+    if((numeros[i] % 2) == 0){
+        somapar += numeros[i];
+    }
+}
+
+console.log(somapar);
