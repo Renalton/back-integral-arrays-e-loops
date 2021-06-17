ada3cb0d5274b82d11098dd3fcbe62bfcd799411
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-17 00:52:55 -0300

    Exercício - Imprima os menores

--- extra-14/index.js
+++ extra-14/index.js
@@ -1,3 +1,12 @@
+//Código para imprimir o menor valor entre dois vetores
+
 const arrayA = [5, 32, 3, 44, 1];
 const arrayB = [57, 4, 21, 2, 13];
 
+for(let i = 0; i < arrayA.length; i++){
+    if(arrayA[i] <= arrayB[i]){
+        console.log(arrayA[i]);
+    }else{
+        console.log(arrayB[i]);
+    }
+}
