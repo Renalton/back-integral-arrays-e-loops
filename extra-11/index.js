b28d2f6c8eedc600433088662cdaf79b13bca2bc
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-17 00:52:20 -0300

    Exercício - Filtrando números

--- extra-11/index.js
+++ extra-11/index.js
@@ -1,2 +1,14 @@
-const original = [5, 7, 13, 17, 26, 34, 118, 245];
+//Código para filtrar os números entre 10 e 20 ou maiores que 100 e armazenar em outro vetor
+const original = [11, 7, 13, 17, 26, 101, 118, 245];
+const vet = [];
 
+
+for(let i of original){
+
+    if((i > 10 && i < 20) || i > 100){
+        vet.push(i);
+    }
+
+}
+
+console.log(vet);
