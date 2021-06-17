a6ffe58d03e6f5f04e5365ad106a42634af26851
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-17 00:48:03 -0300

    Exercício - Contando letras

--- classe-02/index.js
+++ classe-02/index.js
@@ -1,1 +1,12 @@
 const letras = ["A", "a", "B", "C", "E", "e"];
+//variável para contar o número de letras localizadas no vetor
+let aux = 0;
+
+for(let letra of letras){
+
+    if(letra === "E" || letra ==="e"){
+        aux++;
+    }
+}
+
+console.log((aux > 0? `Foram encontradas ${aux} letras "E" ou "e"`: `Nenhuma letra "E" ou "e" foi encontrada.`));
