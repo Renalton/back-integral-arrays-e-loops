66ab7260bd31d82cc820405470cc8e65837ec731
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-17 00:52:37 -0300

    Exercício - Verificando disjuntores

--- extra-13/index.js
+++ extra-13/index.js
@@ -1,2 +1,9 @@
+//código para imprimir o indice do vetor com base nos valores booleanos utilizando o conceito de truthiness
 const disjuntores = [false, false, true, false, false, true, false, false];
 
+for(let i = 0; i < disjuntores.length; i++){
+    
+    if(disjuntores[i]){
+        console.log(i);
+    }
+}
