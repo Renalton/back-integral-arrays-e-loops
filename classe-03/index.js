45dca10e8bb20354f20588b7e351741863b27a41
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-17 00:48:40 -0300

    Exercício - Encontre o 10

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,2 +1,11 @@
-const numeros = [54, 22, 14, 87, 10, 284];
+const numeros = [54, 22, 14, 87, 1, 284];
+//variável para armazenar o indice do vetor
+let index = -1;
 
+for(let i = 0; i < numeros.length; i++){
+    if(numeros[i] === 10){
+        index = i;
+        break;
+    }
+}
+console.log((index === -1? -1: index));
